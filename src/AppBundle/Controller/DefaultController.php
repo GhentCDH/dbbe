982956863e83ed3c1ad9cf1b8aad4e6544cddcb9
Pieterjan De Potter <pieterjan.depotter@ugent.be>
2018-01-23 16:35:42 +0100

    Enable password changes

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -10,6 +10,7 @@ class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @param  Request $request
      */
     public function indexAction(Request $request)
     {
@@ -17,5 +18,15 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
+    }
+
+    /**
+     * Redirect user to the homepage after password reset
+     * @Route("/profile", name="fos_user_profile_show")
+     * @param  Request $request
+     */
+    public function showProfile(Request $request)
+    {
+        return $this->redirectToRoute('homepage');
     }
 }
