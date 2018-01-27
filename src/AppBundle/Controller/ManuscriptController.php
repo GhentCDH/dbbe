8a6719b7f0030800ef2eb46a65931a49e5cc7cd1
Pieterjan De Potter <pieterjan.depotter@ugent.be>
2018-01-27 08:24:56 +0100

    Remove unused import

--- src/AppBundle/Controller/ManuscriptController.php
+++ src/AppBundle/Controller/ManuscriptController.php
@@ -6,7 +6,6 @@ use AppBundle\Exceptions\NotFoundInDatabaseException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 const M_INDEX = 'documents';
