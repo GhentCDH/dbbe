46cc0509a9f309b4fe3eeaf0dd958098097b52dc
Pieterjan De Potter <pieterjan.depotter@ugent.be>
2018-06-01 20:51:18 +0200

    Improve occurrence image display

--- assets/js/main/main.js
+++ assets/js/main/main.js
@@ -1,2 +1,12 @@
 var $ = require('jquery')
 require('bootstrap-sass')
+require('ekko-lightbox')
+
+$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+});
+
+$('.thumbnail.hidden img').on('load', function(event) {
+    $(this).closest('.thumbnail').fadeIn(500).removeClass('hidden');
+});
