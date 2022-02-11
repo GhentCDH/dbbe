7ff25a2bb7076afa2347d64e348dd7b6d158b161
Pieterjan De Potter <pieterjan.depotter@ugent.be>
2022-02-11 15:50:26 +0100

    Update javscript coding style

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,6 +33,24 @@ module.exports = {
                 argsIgnorePattern: '^_',
             },
         ],
+        'no-restricted-syntax': [
+            'error',
+            {
+                selector: 'ForInStatement',
+                // eslint-disable-next-line max-len
+                message: 'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+            },
+            {
+                selector: 'LabeledStatement',
+                // eslint-disable-next-line max-len
+                message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+            },
+            {
+                selector: 'WithStatement',
+                // eslint-disable-next-line max-len
+                message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+            },
+        ],
         'vue/html-indent': [
             'error',
             4,
