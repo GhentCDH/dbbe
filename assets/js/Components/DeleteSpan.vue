0094efa1245b3e7e9c79bcb498df8b5317d71012
cledloof <clement.deloof@ugent.be>
2022-08-09 08:25:36 +0000

    rearranged loop for showing filters for prettier labels

--- assets/js/Components/DeleteSpan.vue
+++ assets/js/Components/DeleteSpan.vue
@@ -1,30 +1,30 @@
 <template>
     <div class="delete-span-box">
-        <div v-if="Array.isArray(this.modelvalue)">
-            <div v-if="this.modelvalue.length">
-                <h4>{{modelkey}}</h4>
-                <div v-for="(val, ind) in modelvalue" :key="val.id" class="delete-span-container">{{val.name}} <i class="fa fa-close delete-span-icon" @click="onDelete(ind)"></i></div>
+        <div v-if="Array.isArray(this.itemvalue)">
+            <div v-if="this.itemvalue.length">
+                <h4>{{itemlabel}}</h4>
+                <div v-for="(val, ind) in itemvalue" :key="val.id" class="delete-span-container">{{val.name}} <i class="fa fa-close delete-span-icon" @click="onDelete(ind)"></i></div>
             </div>
         </div>
-        <div v-else-if="typeof this.modelvalue === 'string' || this.modelkey === 'year_from' || this.modelkey === 'year_to'">
-            <div v-if="this.modelvalue !== ''">
-                <h4>{{modelkey}}</h4>
-                <div class="delete-span-container">{{modelvalue}} <i class="fa fa-close delete-span-icon" @click="onDelete(-1)"></i></div>
+        <div v-else-if="typeof this.itemvalue === 'string' || this.itemkey === 'year_from' || this.itemkey === 'year_to'">
+            <div v-if="this.itemvalue !== ''">
+                <h4>{{itemlabel}}</h4>
+                <div class="delete-span-container">{{itemvalue}} <i class="fa fa-close delete-span-icon" @click="onDelete(-1)"></i></div>
             </div>
         </div>
         <div v-else>
-            <h4>{{modelkey}}</h4>
-            <div class="delete-span-container">{{modelvalue.name}} <i class="fa fa-close delete-span-icon" @click="onDelete(-1)"></i></div>
+            <h4>{{itemlabel}}</h4>
+            <div class="delete-span-container">{{itemvalue.name}} <i class="fa fa-close delete-span-icon" @click="onDelete(-1)"></i></div>
         </div>
     </div>
 </template>
 <script>
 export default {
-    props: ['modelkey', 'modelvalue'],
+    props: ['itemkey', 'itemvalue', 'itemlabel'],
     methods: {
         onDelete(index) {
             this.$emit('deleted', {
-                key: this.modelkey,
+                key: this.itemkey,
                 index: index,
             });
         }
