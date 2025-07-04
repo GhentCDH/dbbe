d4da633347b2985f6f6eabcb31131f5da4c7622f
PaulienLem <paulien.lemay@gmail.com>
2025-07-04 16:00:41 +0200

    Remove AbstractListEdit mixin from acknowledgements

--- assets/js/apps/AcknowledgementsEditApp.vue
+++ assets/js/apps/AcknowledgementsEditApp.vue
@@ -24,8 +24,8 @@
                 <div class="spinner" />
             </div>
         </article>
-        <editModal
-            :show="editModal"
+        <Edit
+            :show="editModalValue"
             :schema="editSchema"
             :submit-model="submitModel"
             :original-submit-model="originalSubmitModel"
@@ -34,7 +34,7 @@
             @reset="resetEdit()"
             @confirm="submitEdit()"
             @dismiss-alert="editAlerts.splice($event, 1)" />
-        <deleteModal
+        <Delete
             :show="deleteModal"
             :del-dependencies="delDependencies"
             :submit-model="submitModel"
@@ -45,21 +45,81 @@
     </div>
 </template>
 
+<template>
+  <div>
+    <article class="col-sm-9 mbottom-large">
+      <Alerts
+          :alerts="alerts"
+          @dismiss="alerts.splice($event, 1)"
+      />
+      <Panel header="Edit acknowledgements">
+        <EditListRow
+            :schema="schema"
+            :model="model"
+            name="acknowledgement"
+            :conditions="{
+            add: true,
+            edit: model.acknowledgement,
+            del: model.acknowledgement
+          }"
+            @add="edit(true)"
+            @edit="edit()"
+            @del="del()"
+        />
+      </Panel>
+      <div v-if="openRequests" class="loading-overlay">
+        <div class="spinner" />
+      </div>
+    </article>
+
+    <Edit
+        :show="editModalValue"
+        :schema="editSchema"
+        :submit-model="submitModel"
+        :original-submit-model="originalSubmitModel"
+        :alerts="editAlerts"
+        @cancel="cancelEdit"
+        @reset="resetEdit(submitModel)"
+        @confirm="submitEdit"
+        @dismiss-alert="editAlerts.splice($event, 1)"
+    />
+
+    <Delete
+        :show="deleteModal"
+        :del-dependencies="delDependencies"
+        :submit-model="submitModel"
+        :alerts="deleteAlerts"
+        @cancel="cancelDelete"
+        @confirm="submitDelete"
+        @dismiss-alert="deleteAlerts.splice($event, 1)"
+    />
+  </div>
+</template>
+
 <script setup>
-import { ref, reactive, watch, computed } from 'vue'
+import { reactive, ref, watch, onMounted } from 'vue'
 import axios from 'axios'
 import VueFormGenerator from 'vue-form-generator'
-import { useEditMergeMigrateDelete } from '@/composables/useEditMergeMigrateDelete'
-import { createMultiSelect, enableField } from '@/helpers/formFieldUtils'
+
 import Edit from '@/Components/Edit/Modals/Edit.vue'
-import Merge from '@/Components/Edit/Modals/Merge.vue'
 import Delete from '@/Components/Edit/Modals/Delete.vue'
 import Panel from '@/Components/Edit/Panel.vue'
-import EditListRow from "@/Components/Edit/EditListRow.vue";
+import Alerts from '@/Components/Alerts.vue'
+import EditListRow from '@/Components/Edit/EditListRow.vue'
+import { isLoginError } from '@/helpers/errorUtil'
+import { useEditMergeMigrateDelete } from '@/composables/useEditMergeMigrateDelete'
+import { createMultiSelect, enableField } from '@/helpers/formFieldUtils'
 
+// Props
 const props = defineProps({
-  initUrls: String,
-  initData: String
+  initUrls: {
+    type: String,
+    required: true
+  },
+  initData: {
+    type: String,
+    required: true
+  }
 })
 
 const {
@@ -75,8 +135,9 @@ const {
   openRequests,
   deleteDependencies,
   cancelEdit,
-  cancelDelete
-} = useEditMergeMigrateDelete(props.initUrls, props.initData, {})
+  cancelDelete,
+  resetEdit
+} = useEditMergeMigrateDelete(props.initUrls, props.initData)
 
 const schema = reactive({
   fields: {
@@ -85,17 +146,17 @@ const schema = reactive({
 })
 
 const editSchema = reactive({
-  fields: {
-    name: {
+  fields: [
+    {
       type: 'input',
       inputType: 'text',
       label: 'Name',
       labelClasses: 'control-label',
       model: 'acknowledgement.name',
       required: true,
-      validator: VueFormGenerator.validators.string,
-    },
-  }
+      validator: VueFormGenerator.validators.string
+    }
+  ]
 })
 
 const model = reactive({
@@ -110,12 +171,22 @@ const submitModel = reactive({
   }
 })
 
-const editModal = computed({
-  get: () => editModalValue.value,
-  set: (val) => editModalValue.value = val
+watch(values, (newValues) => {
+  schema.fields.acknowledgement.values = Array.isArray(newValues) ? newValues : []
+}, { immediate: true })
+
+watch(originalSubmitModel, (newVal) => {
+  Object.assign(submitModel.acknowledgement, newVal.acknowledgement)
 })
 
-const formatType = (type) => type
+watch(values, () => {
+  enableField(schema.fields.acknowledgement)
+})
+
+onMounted(() => {
+  schema.fields.acknowledgement.values = values.value || []
+  enableField(schema.fields.acknowledgement, model)
+})
 
 function edit(add = false) {
   if (add) {
@@ -124,7 +195,7 @@ function edit(add = false) {
     submitModel.acknowledgement = JSON.parse(JSON.stringify(model.acknowledgement))
   }
   Object.assign(originalSubmitModel, JSON.parse(JSON.stringify(submitModel)))
-  editModal.value = true
+  editModalValue.value = true
 }
 
 function del() {
@@ -132,80 +203,84 @@ function del() {
   deleteDependencies()
 }
 
-function submitEdit() {
-  editModal.value = false
+async function update() {
   openRequests.value++
-  const ack = submitModel.acknowledgement
-
-  const successHandler = (response, message) => {
-    submitModel.acknowledgement = response.data
-    update()
-    editAlerts.value = []
-    alerts.value.push({ type: 'success', message })
-    openRequests.value--
-  }
-
-  const errorHandler = (error, message) => {
-    openRequests.value--
-    editModal.value = true
-    editAlerts.value.push({
+  try {
+    const response = await axios.get(urls.acknowledgements_get)
+    if (Array.isArray(values.value)) {
+      values.value.splice(0, values.value.length, ...response.data)
+    }
+    schema.fields.acknowledgement.values = values.value
+    model.acknowledgement = JSON.parse(JSON.stringify(submitModel.acknowledgement))
+  } catch (error) {
+    alerts.value.push({
       type: 'error',
-      message,
+      message: 'Something went wrong while renewing the acknowledgement data.',
       login: isLoginError(error)
     })
     console.error(error)
-  }
-
-  if (ack.id == null) {
-    axios.post(urls.acknowledgement_post, { name: ack.name })
-        .then(res => successHandler(res, 'Addition successful.'))
-        .catch(err => errorHandler(err, 'Something went wrong while adding the acknowledgement.'))
-  } else {
-    axios.put(urls.acknowledgement_put.replace('acknowledgement_id', ack.id), { name: ack.name })
-        .then(res => successHandler(res, 'Update successful.'))
-        .catch(err => errorHandler(err, 'Something went wrong while updating the acknowledgement.'))
+  } finally {
+    openRequests.value--
   }
 }
 
-function submitDelete() {
+async function submitEdit() {
+  editModalValue.value = false
+  openRequests.value++
+
+  try {
+    let response
+    if (submitModel.acknowledgement.id == null) {
+      response = await axios.post(urls.acknowledgement_post, {
+        name: submitModel.acknowledgement.name
+      })
+      submitModel.acknowledgement = response.data
+      alerts.value.push({ type: 'success', message: 'Addition successful.' })
+    } else {
+      response = await axios.put(
+          urls.acknowledgement_put.replace('acknowledgement_id', submitModel.acknowledgement.id),
+          { name: submitModel.acknowledgement.name }
+      )
+      submitModel.acknowledgement = response.data
+      alerts.value.push({ type: 'success', message: 'Update successful.' })
+    }
+    await update()
+    editAlerts.value.splice(0)
+  } catch (error) {
+    editModalValue.value = true
+    editAlerts.value.push({
+      type: 'error',
+      message: submitModel.acknowledgement.id == null
+          ? 'Something went wrong while adding the acknowledgement.'
+          : 'Something went wrong while updating the acknowledgement.',
+      login: isLoginError(error)
+    })
+    console.error(error)
+  } finally {
+    openRequests.value--
+  }
+}
+
+async function submitDelete() {
   deleteModal.value = false
   openRequests.value++
-  axios.delete(urls.acknowledgement_delete.replace('acknowledgement_id', submitModel.acknowledgement.id))
-      .then(() => {
-        submitModel.acknowledgement = null
-        update()
-        deleteAlerts.value = []
-        alerts.value.push({ type: 'success', message: 'Deletion successful.' })
-        openRequests.value--
-      })
-      .catch(error => {
-        openRequests.value--
-        deleteModal.value = true
-        deleteAlerts.value.push({
-          type: 'error',
-          message: 'Something went wrong while deleting the acknowledgement.',
-          login: isLoginError(error)
-        })
-        console.error(error)
-      })
-}
 
-function update() {
-  openRequests.value++
-  axios.get(urls.acknowledgements_get)
-      .then((response) => {
-        values.value = response.data
-        schema.fields.acknowledgement.values = response.data
-        model.acknowledgement = JSON.parse(JSON.stringify(submitModel.acknowledgement))
-        openRequests.value--
-      })
-      .catch((error) => {
-        openRequests.value--
-        alerts.value.push({ type: 'error', message: 'Something went wrong while renewing the acknowledgement data.', login: isLoginError(error) })
-        console.error(error)
-      })
+  try {
+    await axios.delete(urls.acknowledgement_delete.replace('acknowledgement_id', submitModel.acknowledgement.id))
+    submitModel.acknowledgement = null
+    await update()
+    deleteAlerts.value.splice(0)
+    alerts.value.push({ type: 'success', message: 'Deletion successful.' })
+  } catch (error) {
+    deleteModal.value = true
+    deleteAlerts.value.push({
+      type: 'error',
+      message: 'Something went wrong while deleting the acknowledgement.',
+      login: isLoginError(error)
+    })
+    console.error(error)
+  } finally {
+    openRequests.value--
+  }
 }
-
-schema.fields.acknowledgement.values = values.value
-enableField(schema.fields.acknowledgement)
 </script>
--- assets/js/main/acknowledgementsedit.js
+++ assets/js/main/acknowledgementsedit.js
@@ -1,6 +1,13 @@
 import Vue from 'vue';
 import AcknowledgementsEditApp from '@/apps/AcknowledgementsEditApp'
 import VueFormGenerator from 'vue-form-generator'
+import VueMultiselect from 'vue-multiselect';
+import fieldMultiselectClear from '@/Components/FormFields/fieldMultiselectClear.vue';
+import * as uiv from 'uiv';
+
+Vue.use(uiv);
+Vue.component('multiselect', VueMultiselect);
+Vue.component('field-multiselect', fieldMultiselectClear);
 Vue.use(VueFormGenerator);
 
 new Vue({
