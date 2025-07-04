623190e8c2b886ffb805c1455ec036e615fffdff
PaulienLem <paulien.lemay@gmail.com>
2025-07-04 17:04:32 +0200

    refactor rolesedit to use composable

--- assets/js/main/rolesedit.js
+++ assets/js/main/rolesedit.js
@@ -1,6 +1,14 @@
-import Vue from 'vue';
 import RolesEditApp from '@/apps/RolesEditApp'
+import VueFormGenerator from 'vue-form-generator'
+import Vue from 'vue';
+import VueMultiselect from 'vue-multiselect';
+import fieldMultiselectClear from '@/Components/FormFields/fieldMultiselectClear.vue';
+import * as uiv from 'uiv';
 
+Vue.use(uiv);
+Vue.component('multiselect', VueMultiselect);
+Vue.component('field-multiselect', fieldMultiselectClear);
+Vue.use(VueFormGenerator);
 new Vue({
     el: '#roles-edit-app',
     components: {
