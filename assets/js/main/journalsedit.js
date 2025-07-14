c08504d05c93585c63663d0269b7d81c8d44bed1
PaulienLem <paulien.lemay@gmail.com>
2025-07-14 15:16:34 +0200

    Remove mixin for journaledit

--- assets/js/main/journalsedit.js
+++ assets/js/main/journalsedit.js
@@ -1,5 +1,10 @@
 import Vue from 'vue';
 import JournalsEditApp from '@/apps/JournalsEditApp'
+import * as uiv from 'uiv';
+import VueFormGenerator from 'vue-form-generator'
+
+Vue.use(uiv);
+Vue.use(VueFormGenerator);
 
 new Vue({
     el: '#journals-edit-app',
