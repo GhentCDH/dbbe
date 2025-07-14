ce3c32d2b2756e2b66b528dcc48f725f8c08fd47
PaulienLem <paulien.lemay@gmail.com>
2025-07-14 13:54:25 +0200

    Remove mixin from Bookseries

--- assets/js/main/bookseriessedit.js
+++ assets/js/main/bookseriessedit.js
@@ -1,5 +1,10 @@
 import Vue from 'vue';
 import BookSeriessEditApp from '@/apps/BookSeriessEditApp'
+import * as uiv from 'uiv';
+import VueFormGenerator from 'vue-form-generator'
+
+Vue.use(uiv);
+Vue.use(VueFormGenerator);
 
 new Vue({
     el: '#book-seriess-edit-app',
