c1629016c399b634fe1102cac43e07caeff6fc20
PaulienLem <paulien.lemay@gmail.com>
2025-07-14 18:22:39 +0200

    Refactor journalissuesedit

--- assets/js/main/journalissuesedit.js
+++ assets/js/main/journalissuesedit.js
@@ -1,5 +1,14 @@
 import Vue from 'vue';
 import JournalIssuesEditApp from '@/apps/JournalIssuesEditApp'
+import * as uiv from 'uiv';
+import VueMultiselect from 'vue-multiselect';
+import VueFormGenerator from 'vue-form-generator'
+import fieldMultiselectClear from '@/Components/FormFields/fieldMultiselectClear.vue';
+
+Vue.use(uiv);
+Vue.use(VueFormGenerator);
+Vue.component('multiselect', VueMultiselect);
+Vue.component('field-multiselect', fieldMultiselectClear);
 
 new Vue({
     el: '#journal-issues-edit-app',
