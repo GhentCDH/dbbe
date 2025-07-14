16a5de372e8ea7f7902ccbba4434e4044af85abe
PaulienLem <paulien.lemay@gmail.com>
2025-07-14 16:05:15 +0200

    wip refactor locationsedit

--- assets/js/main/locationsedit.js
+++ assets/js/main/locationsedit.js
@@ -1,5 +1,10 @@
 import Vue from 'vue';
 import LocationsEditApp from '@/apps/LocationsEditApp'
+import * as uiv from 'uiv';
+import VueFormGenerator from 'vue-form-generator'
+
+Vue.use(uiv);
+Vue.use(VueFormGenerator);
 
 new Vue({
     el: '#locations-edit-app',
