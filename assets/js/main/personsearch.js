44c402280e8be5cdf7f9b913502ada74581996a4
PaulienLem <paulien.lemay@gmail.com>
2025-08-04 10:55:04 +0200

    Rewrite personsearch for vue3

--- assets/js/main/personsearch.js
+++ assets/js/main/personsearch.js
@@ -1,5 +1,28 @@
 import Vue from 'vue';
 import PersonSearchApp from '@/apps/PersonSearchApp'
+import fieldRadio from '../Components/FormFields/fieldRadio.vue';
+import VueTables from 'vue-tables-2';
+import Alerts from '../Components/Alerts.vue'
+import axios from 'axios';
+import VueFormGenerator from 'vue-form-generator'
+import VueMultiselect from 'vue-multiselect';
+import fieldMultiselectClear from '@/Components/FormFields/fieldMultiselectClear.vue';
+import fieldCheckboxes from '../Components/FormFields/fieldCheckboxes.vue';
+import * as uiv from 'uiv'
+
+Vue.use(uiv);
+Vue.component('multiselect', VueMultiselect);
+Vue.component('field-multiselect', fieldMultiselectClear);
+Vue.use(VueFormGenerator);
+Vue.component('FieldCheckboxes', fieldCheckboxes);
+
+
+Vue.component('FieldRadio', fieldRadio);
+Vue.use(VueTables.ServerTable);
+Vue.component('multiselect', VueMultiselect)
+Vue.component('fieldMultiselectClear', fieldMultiselectClear)
+Vue.component('alerts', Alerts)
+window.axios = axios;
 
 new Vue({
     el: '#person-search-app',
