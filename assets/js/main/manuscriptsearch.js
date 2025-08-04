47dcdf33fc9220212f04d3c2b9eefd94955262a9
PaulienLem <paulien.lemay@gmail.com>
2025-08-04 10:55:35 +0200

    Restructure imports

--- assets/js/main/manuscriptsearch.js
+++ assets/js/main/manuscriptsearch.js
@@ -3,18 +3,21 @@ import ManuscriptSearchApp from '@/apps/ManuscriptSearchApp'
 
 import fieldRadio from '../Components/FormFields/fieldRadio.vue';
 import VueTables from 'vue-tables-2';
-import fieldMultiselectClear from '../Components/FormFields/fieldMultiselectClear.vue'
 import Alerts from '../Components/Alerts.vue'
 import axios from 'axios';
-import VueMultiselect from 'vue-multiselect'
-
-
+import VueFormGenerator from 'vue-form-generator'
+import VueMultiselect from 'vue-multiselect';
+import fieldMultiselectClear from '@/Components/FormFields/fieldMultiselectClear.vue';
+import fieldCheckboxes from '../Components/FormFields/fieldCheckboxes.vue';
 import * as uiv from 'uiv'
-Vue.use(uiv);
 
-Vue.component('FieldRadio', fieldRadio);
+Vue.use(uiv);
 Vue.use(VueTables.ServerTable);
-Vue.component('multiselect', VueMultiselect)
+Vue.use(VueFormGenerator);
+
+Vue.component('multiselect', VueMultiselect);
+Vue.component('FieldCheckboxes', fieldCheckboxes);
+Vue.component('FieldRadio', fieldRadio);
 Vue.component('fieldMultiselectClear', fieldMultiselectClear)
 Vue.component('alerts', Alerts)
 window.axios = axios;
