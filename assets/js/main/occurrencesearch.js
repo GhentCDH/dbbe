fd012ffaaf78aa4db0b44cb8ff483ad3ef4c3ddb
PaulienLem <paulien.lemay@gmail.com>
2025-08-04 10:55:21 +0200

    Rewrite occurrencesearch for vue3

--- assets/js/main/occurrencesearch.js
+++ assets/js/main/occurrencesearch.js
@@ -1,16 +1,22 @@
 import Vue from 'vue';
 import OccurrenceSearchApp from '../apps/OccurrenceSearchApp.vue'; // Root component
-import * as uiv from 'uiv'; // UI library
-import fieldMultiselectClear from "../Components/FormFields/fieldMultiselectClear.vue"; // Custom component
 import Delete from "../Components/Edit/Modals/Delete.vue"; // Custom component
 import CollectionManager from "../Components/Search/CollectionManager.vue"; // Custom component
-import fieldRadio from "../Components/FormFields/fieldRadio.vue"; // Custom component
 import VueCookies from "vue-cookies"; // Cookies library
 import VueFormGenerator from "vue-form-generator"; // Form generator library
-import Alerts from "../Components/Alerts.vue"; // Custom component
-
-import VueMultiselect from 'vue-multiselect'
+import fieldRadio from '../Components/FormFields/fieldRadio.vue';
 import VueTables from 'vue-tables-2';
+import fieldMultiselectClear from '../Components/FormFields/fieldMultiselectClear.vue'
+import Alerts from '../Components/Alerts.vue'
+import axios from 'axios';
+import VueMultiselect from 'vue-multiselect'
+import * as uiv from 'uiv'
+import fieldCheckboxes from "@/Components/FormFields/fieldCheckboxes.vue";
+
+window.axios = axios;
+
+
+Vue.use(uiv);
 Vue.use(VueTables.ServerTable);
 Vue.component('multiselect', VueMultiselect)
 Vue.component('FieldRadio', fieldRadio);
@@ -18,12 +24,15 @@ Vue.component('FieldMultiselectClear', fieldMultiselectClear);
 Vue.component('Alerts', Alerts);
 Vue.component('DeleteModal', Delete);
 Vue.component('CollectionManager', CollectionManager);
+Vue.component('FieldCheckboxes', fieldCheckboxes);
+
 
 // Use plugins
 Vue.use(VueFormGenerator);
 Vue.use(uiv);
 Vue.use(VueCookies);
 
+window.axios = axios;
 
 new Vue({
     el: '#occurrence-search-app',
