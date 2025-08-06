16b6f139600d29ce1208feae0110e0a4a78aed2e
PaulienLem <paulien.lemay@gmail.com>
2025-08-06 15:06:31 +0200

    Bring management panel back to options api for now

--- assets/js/Components/Edit/Panels/Management.vue
+++ assets/js/Components/Edit/Panels/Management.vue
@@ -3,131 +3,132 @@
       :header="header"
       :links="links"
       :reloads="reloads"
-      @reload="emitReload"
+      @reload="reload"
   >
     <vue-form-generator
         ref="form"
         :schema="schema"
         :model="model"
         :options="formOptions"
-        @validated="onValidated"
+        @validated="validated"
     />
   </panel>
 </template>
+<script>
+import Vue from 'vue';
+import {
+  createMultiSelect, disableFields, enableFields,
 
-<script setup>
-import { ref, computed, watch, reactive,defineExpose } from 'vue';
-import { createMultiSelect, disableFields, enableFields } from '@/helpers/formFieldUtils';
-import { calcChanges } from '@/helpers/modelChangeUtil';
-import Panel from '../Panel.vue';
+} from '@/helpers/formFieldUtils';
+import Panel from '../Panel'
+import {calcChanges} from "@/helpers/modelChangeUtil";
 
-const props = defineProps({
-  header: {
-    type: String,
-    default: '',
-  },
-  links: {
-    type: Array,
-    default: () => [],
-  },
-  model: {
-    type: Object,
-    default: () => ({}),
-  },
-  reloads: {
-    type: Array,
-    default: () => [],
-  },
-  keys: {
-    type: Object,
-    default: () => ({
-      managements: { field: 'managements', init: true }
-    }),
-  },
-  values: {
-    type: Array,
-    default: () => [],
-  },
-});
+Vue.component('panel', Panel)
 
-const emit = defineEmits(['reload', 'validated']);
+export default {
 
-const schema = ref({ fields: {} });
-const changes = ref([]);
-const isValid = ref(true);
-const originalModel = ref({});
-const form = ref(null);
-
-const formOptions = {
-  validateAfterChanged: true,
-  validationErrorClass: 'has-error',
-  validationSuccessClass: 'success',
-};
-
-const fields = computed(() => schema.value.fields);
-
-watch(
-    () => props.values,
-    (newValues) => {
-      if (!Array.isArray(newValues) || newValues.length === 0) {
-        return;
-      }
-
-      schema.value.fields = {
-        managements: createMultiSelect(
-            'Management collection',
-            {
-              model: 'managements',
-              values: newValues,
-            },
-            {
-              multiple: true,
-              closeOnSelect: false,
-            }
-        ),
-      };
+  props: {
+    header: {
+      type: String,
+      default: '',
     },
-    { immediate: true }
-);
+    links: {
+      type: Array,
+      default: () => {return []},
+    },
+    model: {
+      type: Object,
+      default: () => {return {}},
+    },
+    reloads: {
+      type: Array,
+      default: () => {return []},
+    },
+    keys: {
+      type: Object,
+      default: () => {
+        return {managements: {field: 'managements', init: true}};
+      },
+    },
+    values: {
+      type: Array,
+      default: () => {return []},
+    },
+  },
+  computed: {
+    fields() {
+      return this.schema.fields
+    }
+  },
+  data() {
+    return {
+      changes: [],
+      formOptions: {
+        validateAfterChanged: true,
+        validationErrorClass: 'has-error',
+        validationSuccessClass: 'success',
+      },
+      isValid: true,
+      originalModel: {},
+      schema: {
+        fields: {}
+      }
+    }
+  },
+  watch: {
+    values: {
+      handler(newValues) {
+        if (newValues && newValues.length > 0) {
+          this.buildSchema()
+        }
+      },
+      immediate: true,
+      deep: true
+    }
+  },
 
-// Methods
-function init() {
-  originalModel.value = JSON.parse(JSON.stringify(props.model));
-  enableFieldsFn();
+  methods: {
+    init() {
+      this.originalModel = JSON.parse(JSON.stringify(this.model));
+      this.enableFields();
+    },
+    buildSchema() {
+      this.schema = {
+        fields: {
+          managements: createMultiSelect(
+              'Management collection',
+              {
+                model: 'managements',
+                values: this.values,
+              },
+              {
+                multiple: true,
+                closeOnSelect: false,
+              }
+          ),
+        }
+      }
+    },
+    reload(type) {
+      if (!this.reloads.includes(type)) {
+        this.$emit('reload', type);
+      }
+    },
+    disableFields(disableKeys) {
+      disableFields(this.keys, this.fields, disableKeys);
+    },
+    enableFields(enableKeys) {
+      enableFields(this.keys, this.fields, this.values, enableKeys);
+    },
+    validated(isValid, errors) {
+      this.isValid = isValid
+      this.changes = calcChanges(this.model, this.originalModel, this.fields);
+      this.$emit('validated', isValid, this.errors, this)
+    },
+    validate() {
+      this.$refs.form.validate()
+    },
+  },
+
 }
-
-function emitReload(type) {
-  if (!props.reloads.includes(type)) {
-    emit('reload', type);
-  }
-}
-
-function disableFieldsFn(disableKeys) {
-  disableFields(props.keys, fields.value, disableKeys);
-}
-
-function enableFieldsFn(enableKeys) {
-  enableFields(props.keys, fields.value, props.values, enableKeys);
-}
-
-function onValidated(valid, errors) {
-  isValid.value = valid;
-  changes.value = calcChanges(props.model, originalModel.value, fields.value);
-  emit('validated', valid, errors, {
-    validate,
-    changes: changes.value,
-    schema: schema.value,
-  });
-}
-
-function validate() {
-  form.value?.validate();
-}
-
-defineExpose({
-  init,
-  changes,
-  validate,
-  isValid
-});
 </script>
