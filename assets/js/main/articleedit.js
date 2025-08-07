911b3f9b3ee7b083cc509ace88863dffb69fd63a
PaulienLem <paulien.lemay@gmail.com>
2025-08-07 16:27:18 +0200

    Refactor article

--- assets/js/main/articleedit.js
+++ assets/js/main/articleedit.js
@@ -1,5 +1,14 @@
 import Vue from 'vue';
 import ArticleEditApp from '@/apps/ArticleEditApp'
+import * as uiv from 'uiv'
+import VueFormGenerator from 'vue-form-generator'
+import fieldMultiselectClear from '@/Components/FormFields/fieldMultiselectClear.vue';
+import VueMultiselect from 'vue-multiselect';
+
+Vue.use(uiv);
+Vue.use(VueFormGenerator);
+Vue.component('field-multiselectClear', fieldMultiselectClear)
+Vue.component('multiselect', VueMultiselect);
 
 new Vue({
     el: '#article-edit-app',
