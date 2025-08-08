a5dbe8bda048a3653002df7707c759b6562bc49a
PaulienLem <paulien.lemay@gmail.com>
2025-08-08 08:44:43 +0200

    Remove abstractField mixin

--- assets/js/Components/FormFields/fieldMultiselectClear.vue
+++ assets/js/Components/FormFields/fieldMultiselectClear.vue
@@ -52,16 +52,56 @@ div(@keyup.esc.stop.prevent="onEscStopPrevent")
       span.badge(v-if="props.option.count != null") {{ props.option.count }}
 </template>
 <script>
-import { abstractField } from 'vue-form-generator';
 
+import { get as objGet, forEach, isFunction, isString, isArray, debounce, uniqueId, uniq as arrayUniq } from "lodash";
+import validatorUtil from "@/helpers/validatorUtil";
 function isEmpty(opt) {
     if (opt === 0) return false;
     if (Array.isArray(opt) && opt.length === 0) return true;
     return !opt;
 }
 
+function convertValidator(validator) {
+  if (isString(validator)) {
+    if (validatorUtil[validator] != null) return validatorUtil[validator];
+    else {
+      console.warn(`'${validator}' is not a validator function!`);
+      return null; // caller need to handle null
+    }
+  }
+  return validator;
+}
+
+function attributesDirective(el, binding, vnode) {
+  let attrs = objGet(vnode.context, "schema.attributes", {});
+  let container = binding.value || "input";
+  if (isString(container)) {
+    attrs = objGet(attrs, container) || attrs;
+  }
+  forEach(attrs, (val, key) => {
+    el.setAttribute(key, val);
+  });
+}
+
+
 export default {
-    mixins: [abstractField],
+    props: ["vfg", "model", "schema", "formOptions", "disabled"],
+
+    data() {
+      return {
+        errors: [],
+        debouncedValidateFunc: null,
+        debouncedFormatFunc: null
+      };
+    },
+
+    directives: {
+      attributes: {
+        bind: attributesDirective,
+        updated: attributesDirective,
+        componentUpdated: attributesDirective
+      }
+    },
     computed: {
         selectOptions() {
             return this.schema.selectOptions || {};
@@ -82,12 +122,34 @@ export default {
             ) {
                 return this.schema.selectOptions.customLabel;
             }
-            // this will let the multiselect library use the default behavior if customLabel is not specified
             return undefined;
         },
+      value: {
+        cache: false,
+        get() {
+          let val;
+          if (isFunction(objGet(this.schema, "get"))) {
+            val = this.schema.get(this.model);
+          } else {
+            val = objGet(this.model, this.schema.model);
+          }
+
+          return this.formatValueToField(val);
+        },
+
+        set(newValue) {
+          let oldValue = this.value;
+          newValue = this.formatValueToModel(newValue);
+
+          if (isFunction(newValue)) {
+            newValue(newValue, oldValue);
+          } else {
+            this.updateModelValue(newValue, oldValue);
+          }
+        }
+      }
     },
     created() {
-        // Check if the component is loaded globally
         if (!this.$root.$options.components.multiselect) {
             // eslint-disable-next-line max-len
             console.error("'vue-multiselect' is missing. Please download from https://github.com/monterail/vue-multiselect and register the component globally!");
@@ -131,9 +193,149 @@ export default {
             if (isEmpty(label)) return '';
             return label;
         },
-        onEscStopPrevent(/*event*/) {
-            // console.log(event);
-        },
+      validate(calledParent) {
+        this.clearValidationErrors();
+        let validateAsync = objGet(this.formOptions, "validateAsync", false);
+
+        let results = [];
+
+        if (this.schema.validator && this.schema.readonly !== true && this.disabled !== true) {
+          let validators = [];
+          if (!isArray(this.schema.validator)) {
+            validators.push(convertValidator(this.schema.validator).bind(this));
+          } else {
+            forEach(this.schema.validator, validator => {
+              validators.push(convertValidator(validator).bind(this));
+            });
+          }
+
+          forEach(validators, validator => {
+            if (validateAsync) {
+              results.push(validator(this.value, this.schema, this.model));
+            } else {
+              let result = validator(this.value, this.schema, this.model);
+              if (result && isFunction(result.then)) {
+                result.then(err => {
+                  if (err) {
+                    this.errors = this.errors.concat(err);
+                  }
+                  let isValid = this.errors.length === 0;
+                  this.$emit("validated", isValid, this.errors, this);
+                });
+              } else if (result) {
+                results = results.concat(result);
+              }
+            }
+          });
+        }
+
+        let handleErrors = (errors) => {
+          let fieldErrors = [];
+          forEach(arrayUniq(errors), err => {
+            if (isArray(err) && err.length > 0) {
+              fieldErrors = fieldErrors.concat(err);
+            } else if (isString(err)) {
+              fieldErrors.push(err);
+            }
+          });
+          if (isFunction(this.schema.onValidated)) {
+            this.schema.onValidated.call(this, this.model, fieldErrors, this.schema);
+          }
+
+          let isValid = fieldErrors.length === 0;
+          if (!calledParent) {
+            this.$emit("validated", isValid, fieldErrors, this);
+          }
+          this.errors = fieldErrors;
+          return fieldErrors;
+        };
+
+        if (!validateAsync) {
+          return handleErrors(results);
+        }
+
+        return Promise.all(results).then(handleErrors);
+      },
+
+      debouncedValidate() {
+        if (!isFunction(this.debouncedValidateFunc)) {
+          this.debouncedValidateFunc = debounce(
+              this.validate.bind(this),
+              objGet(this.schema, "validateDebounceTime", objGet(this.formOptions, "validateDebounceTime", 500))
+          );
+        }
+        this.debouncedValidateFunc();
+      },
+
+      updateModelValue(newValue, oldValue) {
+        let changed = false;
+        if (isFunction(this.schema.set)) {
+          this.schema.set(this.model, newValue);
+          changed = true;
+        } else if (this.schema.model) {
+          this.setModelValueByPath(this.schema.model, newValue);
+          changed = true;
+        }
+
+        if (changed) {
+          this.$emit("model-updated", newValue, this.schema.model);
+
+          if (isFunction(this.schema.onChanged)) {
+            this.schema.onChanged.call(this, this.model, newValue, oldValue, this.schema);
+          }
+
+          if (objGet(this.formOptions, "validateAfterChanged", false) === true) {
+            if (objGet(this.schema, "validateDebounceTime", objGet(this.formOptions, "validateDebounceTime", 0)) > 0) {
+              this.debouncedValidate();
+            } else {
+              this.validate();
+            }
+          }
+        }
+      },
+
+      clearValidationErrors() {
+        this.errors.splice(0);
+      },
+
+      setModelValueByPath(path, value) {
+        let s = path.replace(/\[(\w+)\]/g, ".$1");
+
+        // strip a leading dot
+        s = s.replace(/^\./, "");
+
+        let o = this.model;
+        const a = s.split(".");
+        let i = 0;
+        const n = a.length;
+        while (i < n) {
+          let k = a[i];
+          if (i < n - 1)
+            if (o[k] !== undefined) {
+              // Found parent property. Step in
+              o = o[k];
+            } else {
+              // Create missing property (new level)
+              this.$root.$set(o, k, {});
+              o = o[k];
+            }
+          else {
+            // Set final property value
+            this.$root.$set(o, k, value);
+            return;
+          }
+
+          ++i;
+        }
+      },
+
+      formatValueToField(value) {
+        return value;
+      },
+
+      formatValueToModel(value) {
+        return value;
+      }
     },
 };
 </script>
