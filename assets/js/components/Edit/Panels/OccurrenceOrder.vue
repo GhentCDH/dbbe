b3632f62138eddfb5082290ed7ce4d5c5db9971b
PaulienLem <paulien.lemay@gmail.com>
2025-09-09 13:27:08 +0200

    fix changing occ order

--- assets/js/components/Edit/Panels/OccurrenceOrder.vue
+++ assets/js/components/Edit/Panels/OccurrenceOrder.vue
@@ -1,21 +1,22 @@
 <template>
-    <panel :header="header">
-        <draggable
-            v-model="model.occurrenceOrder"
-            @change="onChange">
-            <transition-group>
-                <div
-                    class="panel panel-default draggable-item"
-                    v-for="occurrence in model.occurrenceOrder"
-                    :key="occurrence.id">
-                    <div class="panel-body">
-                        <i class="fa fa-arrows draggable-icon" />[{{ occurrence.id }}] <span class="greek">{{ occurrence.name }}</span> ({{ occurrence.location}})
-                    </div>
-                </div>
-            </transition-group>
-        </draggable>
-    </panel>
+  <panel :header="header">
+    <draggable
+        v-model="model.occurrenceOrder"
+        @change="onChange">
+      <transition-group>
+        <div
+            class="panel panel-default draggable-item"
+            v-for="occurrence in model.occurrenceOrder"
+            :key="occurrence.id">
+          <div class="panel-body">
+            <i class="fa fa-arrows draggable-icon" />[{{ occurrence.id }}] <span class="greek">{{ occurrence.name }}</span> ({{ occurrence.location}})
+          </div>
+        </div>
+      </transition-group>
+    </draggable>
+  </panel>
 </template>
+
 <script>
 import Vue from 'vue';
 import draggable from 'vuedraggable'
@@ -27,7 +28,7 @@ Vue.component('panel', Panel)
 Vue.component('draggable', draggable)
 
 export default {
-    props: {
+  props: {
     header: {
       type: String,
       default: '',
@@ -53,56 +54,55 @@ export default {
       default: () => {return {}},
     },
   },
-    computed: {
-        fields() {
-            return {
-                occurrenceOrder: {
-                    label: 'Occurrence Order',
-                },
-            }
-        }
-    },
-    data() {
+  computed: {
+    fields() {
       return {
-        changes: [],
-        formOptions: {
-          validateAfterChanged: true,
-          validationErrorClass: 'has-error',
-          validationSuccessClass: 'success',
+        occurrenceOrder: {
+          label: 'Occurrence Order',
         },
-        isValid: true,
-        originalModel: {}
+      }
+    }
+  },
+  data() {
+    return {
+      changes: [],
+      formOptions: {
+        validateAfterChanged: true,
+        validationErrorClass: 'has-error',
+        validationSuccessClass: 'success',
+      },
+      isValid: true,
+      originalModel: {}
+    }
+  },
+  methods: {
+    validate() {
+      this.changes = calcChanges(this.model, this.originalModel, this.fields);
+    },
+    onChange() {
+      this.changes = calcChanges(this.model, this.originalModel, this.fields);
+      this.$emit('validated', this.isValid, [], this);
+    },
+    init() {
+      this.originalModel = JSON.parse(JSON.stringify(this.model));
+      this.enableFields();
+    },
+    reload(type) {
+      if (!this.reloads.includes(type)) {
+        this.$emit('reload', type);
       }
     },
-    methods: {
-        validate() {
-            calcChanges();
-        },
-        onChange() {
-            calcChanges();
-            this.$emit('validated')
-        },
-        init() {
-          this.originalModel = JSON.parse(JSON.stringify(this.model));
-          this.enableFields();
-        },
-        reload(type) {
-          if (!this.reloads.includes(type)) {
-            this.$emit('reload', type);
-          }
-        },
-        disableFields(disableKeys) {
-          disableFields(this.keys, this.fields, disableKeys);
-        },
-        enableFields(enableKeys) {
-          enableFields(this.keys, this.fields, this.values, enableKeys);
-        },
-        validated(isValid, errors) {
-          this.isValid = isValid
-          this.changes = calcChanges(this.model, this.originalModel, this.fields);
-          this.$emit('validated', isValid, this.errors, this)
-        },
-
-    }
+    disableFields(disableKeys) {
+      disableFields(this.keys, this.fields, disableKeys);
+    },
+    enableFields(enableKeys) {
+      enableFields(this.keys, this.fields, this.values, enableKeys);
+    },
+    validated(isValid, errors) {
+      this.isValid = isValid
+      this.changes = calcChanges(this.model, this.originalModel, this.fields);
+      this.$emit('validated', isValid, errors, this)
+    },
+  }
 }
 </script>
