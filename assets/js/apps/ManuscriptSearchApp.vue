77b865356273aedb0b5d415313311c6bd6568c5e
PaulienLem <paulien.lemay@gmail.com>
2025-09-17 13:51:25 +0200

    Remove logging

--- assets/js/apps/ManuscriptSearchApp.vue
+++ assets/js/apps/ManuscriptSearchApp.vue
@@ -660,16 +660,10 @@ const requestFunction = async (requestData) => {
   );
 };
 
-// Add these console logs
-console.log('aggregationLoaded initial value:', aggregationLoaded.value);
-console.log('urlInitialized initial value:', urlInitialized.value);
-
 watch(
     () => aggregationLoaded.value,
     (loaded) => {
-      console.log('Watch triggered - aggregationLoaded:', loaded, 'urlInitialized:', urlInitialized.value);
       if (loaded && !urlInitialized.value) {
-        console.log('About to call initFromURL');
         initFromURL(aggregation.value);
         urlInitialized.value = true;
         initialized.value = true;
