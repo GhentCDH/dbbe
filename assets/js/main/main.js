1c3084341e7419f2cda6d4c26a71a676ea743cdf
PaulienLem <paulien.lemay@gmail.com>
2026-01-29 16:23:27 +0100

    switch back to js

--- assets/js/main/main.js
+++ assets/js/main/main.js
@@ -12,16 +12,6 @@ Promise.all([
 import 'mark.js/dist/jquery.mark.js';
 import '../../scss/screen.scss'
 import '@firstandthird/toc/dist/toc.js';
-import Vue from "vue";
-import VueMatomo from "vue-matomo";
-Vue.use(VueMatomo, {
-    host: 'https://analytics.ghentcdh.ugent.be/',
-    siteId: 4,
-    enableLinkTracking: true,
-    trackInitialView: true,
-    debug: false,
-});
-
 
 (function() {
     $(document).on('click', '[data-toggle="lightbox"]', function (event) {
